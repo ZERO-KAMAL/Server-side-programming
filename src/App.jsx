37172a20b98e8a0a98b0d7ebb33c07f39f7110d0
Khatri <Kamal.Khatri@seamk.fi>
2026-01-08 10:23:12 +0200

    initilize project

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+const  App = () => {
+  // const [count, setCount] = useState(0)
+//  console.log("hello world")
+
+const now = new Date()
+const a = 10
+const b = 20
+console.log(now, a+b)
+  return (
+    <>
+      <div>
+      <p>Hello world, it is {now.toString()}</p>
+      <p>
+        {a} plus {b} is {a + b}
+      </p>
+    </div>
+    </>
+  )
+}
+
+export default App
